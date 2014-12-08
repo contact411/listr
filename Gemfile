860cc35829ae197c18f4732437a8d68d5302e220
Joel Hampton <joelhampton@gmail.com>
2014-12-08 17:43:01 +1100

    Joel & Lenny - Added new Heroku, Mandrill, and installed Devise.. We promise to commit more often.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'paperclip'
 
 gem 'aws-sdk'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'attachments/show'
 
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
