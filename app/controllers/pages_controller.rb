fb6105832cf3bf57ffe7ba4708dd9267d0425ff5
Joel Hampton <joelhampton@gmail.com>
2014-12-12 13:07:57 +1100

    flipped the order of newest listings on the home screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
     @categories = Category.all
-    @listings = Listing.order(:updated_at).limit(3)
+    @listings = Listing.reverse_order(:updated_at).limit(3)
   end
 end
